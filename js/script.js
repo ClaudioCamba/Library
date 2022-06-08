81f948948215f2b30a48784932b23660dd5a9ec3
Claudio Camba <claudio.a.jose@gmail.com>
2022-06-08 12:01:02 +0100

    Add encode and decode functions to assist w/ using title as data attribute

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ function hideShowModal() {
     }
 }
 
+// Encode characters
+function encodeHTML(s) {
+    return s.split('&').join('&amp;').split('<').join('&lt;').split('"').join('&quot;').split("'").join('&#39;');
+}
+// Decode characters
+function decodeHTML(s) {
+    return s.split('&amp;').join('&').split('&lt;').join('<').split('&quot;').join('"').split('&#39;').join("'");
+}
+
+
+
 // Book Function
 function Book(title, author, pages, read) {
     this.title = title
@@ -29,21 +40,40 @@ Book.prototype.createCard = function () {
     listElem.innerHTML += '<p>' + this.author + '</p>';
     listElem.innerHTML += '<p>' + this.pages + '</p>';
     listElem.innerHTML += '<p>' + this.read + '</p>';
+    listElem.setAttribute('data-title', encodeHTML(this.title));
     bookShelf.prepend(listElem);
 }
 
 function addBooktoLibrary() {
-    const book = new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value);
-    myLibrary.unshift(book);
-    book.createCard();
+
+    let book = new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value);
+
+    myLibrary.push(book); // Add book to Array
+    book.createCard(); // Add book to page
+    // book.createCard();
     // for (const book of myLibrary) {
     //     const listElement = document.createElement("li");
     //     listElement.innerText = book;
     //     bookShelf.appendChild(listElement);
     // }
-
 }
 
+// function showBooksOnPage() {
+//     for (const book of myLibrary) {
+//         book.createCard();
+//     }
+// }
+
+// function checkLibrary() {
+
+// }
+
+// let test = document.querySelectorAll('.lib-container li h4')
+
+
+
 // IMPROVEMENTS ============================================
+// Change pages to numbers and read into radio
+// Clear form input values
 // Check if book already exists
 // Only add if sections have been filled
